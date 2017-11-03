ae3ca8eaa0165b323b6c9b8d2b6c4125ce9ef5f1
AfreebreyQA <andrew.freebrey@qa.com>
2017-11-03 15:44:36 +0000

    Added return to home on pages. Have things outputed to page on previous exercises

--- JavaScript_Course/Excersises/Level2_Intermediate/1.Iteration3-FizzBuzz.js
+++ JavaScript_Course/Excersises/Level2_Intermediate/1.Iteration3-FizzBuzz.js
@@ -1,25 +1,37 @@
 function fizzBuzz(_num, _div1, _div2) {
-    for (let i = 1; i <= _num; i++) {
-        if (i % _div1 == 0 & i % _div2 == 0) {
-            console.log("FizzBuzz");
-            continue;
-        }
-        else if(i% _div1 == 0){
-            console.log("Fizz");
-        }
-        else if(i%_div2 == 0){
-            console.log("Buzz");
-        }
-        else{
-            console.log(i);
-        }
+
+    let outputDiv = document.getElementById("outputDiv");
+
+    let output;
+    if (output = document.getElementById("output")) {
+        outputDiv.removeChild(output);
     }
+    output = document.createElement("div");
+    output.setAttribute("id", "output");
+    for (let i = 1; i <= _num; i++) {
+        let anOutput;
+        anOutput = i;
+
+        if (i % _div1 == 0) {
+            anOutput = "Fizz";
+        }
+        if (i % _div2 == 0) {
+            anOutput = "Buzz";
+        }
+        if (i % _div1 == 0 & i % _div2 == 0) {
+            anOutput = "FizzBuzz";
+        }
+
+        output.appendChild(document.createTextNode(anOutput));
+        output.appendChild(document.createElement("br"));
+    }
+    outputDiv.appendChild(output);
 }
 
-function submit(){
+function submit() {
     let num = document.getElementById("num").value;
-    let div1= document.getElementById("div1").value;
-    let div2= document.getElementById("div2").value;
+    let div1 = document.getElementById("div1").value;
+    let div2 = document.getElementById("div2").value;
 
-    fizzBuzz(num,div1,div2);
+    fizzBuzz(num, div1, div2);
 }
--- JavaScript_Course/Excersises/Level2_Intermediate/2.Iteration4.js
+++ JavaScript_Course/Excersises/Level2_Intermediate/2.Iteration4.js
@@ -1,22 +1,36 @@
+let outputDiv;
+let output;
+
 function divide3(_num, _count = 0) {
+
+    let anOutput = document.createElement("p");
     if (_num == 1) {
+        anOutput.innerHTML = "Dividing has reached 1 <br> It took " + _count + " iterations <br>";
+        output.appendChild(anOutput);
+        
         console.log("Dividing has reached 1");
         console.log("It took " + _count + " iterations");
         return;
     }
     if (_num % 3 == 0) {
+        anOutput.innerHTML = "Dividing " + _num + " by 3";
+        output.appendChild(anOutput);
         console.log("Dividing " + _num + " by 3");
         console.log(_num / 3);
         _count++;
         divide3(_num / 3, _count);
     }
     else if ((_num + 1) % 3 == 0) {
+        anOutput.innerHTML = "Adding 1 to " + _num + " then dividing by 3";
+        output.appendChild(anOutput);
         console.log("Adding 1 to " + _num + " then dividing by 3");
         console.log((_num + 1) / 3);
         _count++;
         divide3((_num + 1) / 3, _count);
     }
     else {
+        anOutput.innerHTML = "Subtracting 1 to " + _num + " then dividing by 3";
+        output.appendChild(anOutput);
         console.log("Subtracting 1 to " + _num + " then dividing by 3");
         console.log((_num - 1) / 3);
         _count++;
@@ -24,7 +38,18 @@ function divide3(_num, _count = 0) {
     }
 }
 
-function submit(){
+function submit() {
+    outputDiv = document.getElementById("outputDiv")
+
+    if (output = document.getElementById("document")) {
+        outputDiv.removeChild(output);
+    }
+
+    output = document.createElement("div");
+    output.setAttribute("id", "output");
+
     let num = document.getElementById("input").value;
     divide3(num);
+
+    outputDiv.appendChild(output);
 }
