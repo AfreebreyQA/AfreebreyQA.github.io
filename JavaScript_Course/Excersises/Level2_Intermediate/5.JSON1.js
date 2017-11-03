73b737641bb7b92002fc9bd8da208ee6f8d57076
AfreebreyQA <andrew.freebrey@qa.com>
2017-11-03 14:14:54 +0000

    Finished the JSON1 excersice and inserted bootstrap start

--- JavaScript_Course/Excersises/Level2_Intermediate/5.JSON1.js
+++ JavaScript_Course/Excersises/Level2_Intermediate/5.JSON1.js
@@ -1,9 +1,11 @@
-function DisplayData(data) {
+let JSONData;
+
+function logData(data) {
     for (let key in data) {
         let currentData = data[key];
         if (typeof currentData === "object") {
             console.log(key, ":");
-            DisplayData(currentData);
+            logData(currentData);
         }
         else {
             console.log(key, ":", currentData);
@@ -12,7 +14,53 @@ function DisplayData(data) {
     console.log("");
 }
 
+function DisplayData() {
+document.getElementById("JSON1").removeChild(document.getElementById("getDataButton"));
 
+    document.getElementById("squadNameOutput").innerHTML = JSONData["squadName"];
+    document.getElementById("homeTownOutput").innerHTML = JSONData["homeTown"];
+    document.getElementById("formedOutput").innerHTML = JSONData["formed"];
+    document.getElementById("secretBaseOutput").innerHTML = JSONData["secretBase"];
+    document.getElementById("activeOutput").innerHTML = JSONData["active"];
+    let members = JSONData["members"]
+    for (let key in members) {
+        // store current member
+        let member = members[key];
+
+        let aMemberDiv = document.createElement("div");
+        aMemberDiv.setAttribute("name", aMemberDiv);
+        // iterate through member attribute
+        for (let membersKey in member) {
+            let memberAttribute = member[membersKey]
+            let attributeTitle = document.createElement("h3");
+            attributeTitle.innerHTML = membersKey;
+            aMemberDiv.appendChild(attributeTitle);
+            // if dealing with member's power
+            if (membersKey == "powers") {
+                let powers = memberAttribute;
+                let powerDiv = document.createElement("div");
+                for (power in powers) {
+                    let thisPower = powers[power];
+                    let thisPowerOutput = document.createTextNode(thisPower);
+                    powerDiv.appendChild(thisPowerOutput);
+                    powerDiv.appendChild(document.createElement("br"));
+                }
+                aMemberDiv.appendChild(powerDiv);
+                aMemberDiv.appendChild(document.createElement("br"));
+            }
+            else {
+                let attributOutput = document.createTextNode(member[membersKey]);
+                aMemberDiv.appendChild(attributOutput);
+            }
+        }
+
+        document.getElementById("JSONDisplayDiv").appendChild(aMemberDiv);
+    }
+
+
+
+
+}
 
 function JSON1() {
     let requestURL = "https://raw.githubusercontent.com/ewomackQA/JSONDataRepo/master/example.json";
@@ -21,6 +69,8 @@ function JSON1() {
     request.responseType = "json";
     request.send();
     request.onload = function () {
-        DisplayData(request.response)
+        JSONData = request.response;
+        logData(request.response)
+        DisplayData();
     }
 }
